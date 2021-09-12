db884a0c59bc465e7a08e52c938cc3474561028e
Raito Bezarius <masterancpp@gmail.com>
2021-09-12 20:05:54 +0200

    dotfiles: add i3 & i3status-rs

--- dotfiles.nix
+++ dotfiles.nix
@@ -3,4 +3,8 @@
   home.file.".config/sway/config".source = ./dotfiles/sway;
   home.file.".config/waybar/config".source = ./dotfiles/waybar/config;
   home.file.".config/waybar/style.css".source = ./dotfiles/waybar/style.css;
+
+  home.file.".config/i3/config".source = ./dotfiles/i3/config;
+  home.file.".config/i3status-rs/config-top.toml".source = ./dotfiles/i3status-rs/config-top.toml;
+  home.file.".config/i3status-rs/config-bottom.toml".source = ./dotfiles/i3status-rs/config-bottom.toml;
 }

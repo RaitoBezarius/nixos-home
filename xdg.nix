035d7ff815e7538f0bf32d7f57ae3452c9674601
Raito Bezarius <masterancpp@gmail.com>
2022-03-13 22:00:33 +0100

    xdg: fix chromium for http/https and mutt for mailto

--- xdg.nix
+++ xdg.nix
@@ -7,6 +7,11 @@
     associations.added = {
       "application/pdf" = [ "zathura.desktop" ];
       "image/jpeg" = [ "feh.desktop" ];
+      "x-scheme-handler/mailto" = [ "mutt.desktop" ];
+    };
+    defaultApplications = {
+      "x-scheme-handler/http" = "chromium.desktop";
+      "x-scheme-handler/https" = "chromium.desktop";
     };
   };
 }

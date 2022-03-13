f74da5f0769d49493ded18a74952c0063afa915e
Raito Bezarius <masterancpp@gmail.com>
2022-03-13 22:00:01 +0100

    chore: update neovim configuration

--- neovim.nix
+++ neovim.nix
@@ -65,6 +65,7 @@ in
       oceanic-next
       editorconfig-vim
       vim-python-pep8-indent
+      vim-cue
     ];
     extraPackages = with pkgs; [
       (python3.withPackages (ps: with ps; [
@@ -80,12 +81,40 @@ in
 
     ]);
     extraConfig = ''
-      " your custom vimrc
+      " Classical options
       set nocompatible
       set backspace=indent,eol,start
       set nospell
+      set hidden
       set clipboard+=unnamedplus
+      set expandtab
       colorscheme OceanicNext
+
+      " Some servers have issues with backup files, see #649.
+      set nobackup
+      set nowritebackup
+
+      " Give more space for displaying messages.
+      set cmdheight=2
+
+      " Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+      " delays and poor user experience.
+      set updatetime=300
+
+      " Don't pass messages to |ins-completion-menu|.
+      set shortmess+=c
+
+      " Always show the signcolumn, otherwise it would shift the text each time
+      " diagnostics appear/become resolved.
+      if has("nvim-0.5.0") || has("patch-8.1.1564")
+        " Recently vim can merge signcolumn and number column into one
+        set signcolumn=number
+      else
+        set signcolumn=yes
+      endif
+
+      " <c-space> autocompletes
+      inoremap <silent><expr> <c-space> coc#refresh()
 
       let mapleader = ","
       " toggle line numbers
@@ -110,6 +139,35 @@ in
 
       " Use <leader>x for convert visual selected code to snippet
       xmap <leader>x  <Plug>(coc-convert-snippet)
+
+      " GoTo code navigation.
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)
+
+      " Use K to show documentation in preview window.
+      nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+      function! s:show_documentation()
+        if (index(['vim','help'], &filetype) >= 0)
+          execute 'h '.expand('<cword>')
+        elseif (coc#rpc#ready())
+          call CocActionAsync('doHover')
+        else
+          execute '!' . &keywordprg . " " . expand('<cword>')
+        endif
+      endfunction
+
+      " Highlight the symbol and its references when holding the cursor.
+      autocmd CursorHold * silent call CocActionAsync('highlight')
+
+      " Symbol renaming.
+      nmap <leader>rn <Plug>(coc-rename)
+
+      " Formatting selected code.
+      xmap <leader>f  <Plug>(coc-format-selected)
+      nmap <leader>f  <Plug>(coc-format-selected)
 
       " For pandoc
       let g:pandoc#modules#disabled = ["spell"]

ffa705cfd822bc2545a996360ee85bc99193c6ee
Raito Bezarius <masterancpp@gmail.com>
2022-08-14 01:20:38 +0200

    git: add ghq usage

--- shell/git.nix
+++ shell/git.nix
@@ -4,10 +4,15 @@ let
 in
 {
   # Git
+  home.packages = [ pkgs.ghq ];
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull; # for git send-email
     userEmail = emailUtils.obfuscate "moc.liamg@ppcnaretsam";
     userName = emailUtils.obfuscate "suirazeB otiaR";
+
+    extraConfig.ghq = {
+      root = "~/dev";
+    };
   };
 }

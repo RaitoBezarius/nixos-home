3271debbd550d7f59e75b26f0fbe0dd941621216
Raito Bezarius <masterancpp@gmail.com>
2022-12-04 23:54:37 +0100

    gpg: have a smart pinentry program depending on SSH or not

--- gpg.nix
+++ gpg.nix
@@ -1,13 +1,36 @@
 { config, pkgs, ... }:
+let
+smart-pinentry = pkgs.writeScriptBin "pinentry" ''
+        #!${pkgs.runtimeShell}
+        # choose pinentry depending on PINENTRY_USER_DATA
+        # requires pinentry-curses and pinentry-gtk2
+        # this *only works* with gpg 2
+        # see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=802020
+        
+        case $PINENTRY_USER_DATA in
+        gtk)
+          exec ${pkgs.pinentry.gtk2}/bin/pinentry "$@"
+          ;;
+        none)
+          exit 1 # do not ask for passphrase
+          ;;
+        *)
+          exec ${pkgs.pinentry.curses}/bin/pinentry "$@"
+        esac
+        '';
+  in
 {
   programs.gpg = {
     enable = true;
   };
-
-  home.file.".gnupg/gpg-agent.conf".text = ''
-    pinentry-program = "${pkgs.pinentry_qt5}/bin/pinentry-qt";
-  '';
-
+  services.gpg-agent = {
+    enable = true;
+    enableZshIntegration = true;
+    pinentryFlavor = null;
+    extraConfig = ''
+      pinentry-program ${smart-pinentry}/bin/pinentry
+    '';
+  };
   # Browser Pass
   programs.browserpass = {
     enable = true;

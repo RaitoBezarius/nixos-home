81fd31917f1089d5290ec2635b925f2ecbae383a
Raito Bezarius <masterancpp@gmail.com>
2022-12-05 03:03:05 +0100

    gpg: fix the pinentry stuff

--- gpg.nix
+++ gpg.nix
@@ -9,7 +9,7 @@ smart-pinentry = pkgs.writeScriptBin "pinentry" ''
         
         case $PINENTRY_USER_DATA in
         gtk)
-          exec ${pkgs.pinentry-qt}/bin/pinentry-qt "$@"
+          exec ${pkgs.pinentry-gtk2}/bin/pinentry "$@"
           ;;
         none)
           exit 1 # do not ask for passphrase

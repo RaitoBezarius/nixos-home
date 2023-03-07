f0f7cb476eea163fe59cc3124bcb6a50ec30bc4b
Raito Bezarius <masterancpp@gmail.com>
2023-03-07 09:29:36 +0100

    vscode: clean up configuration with upstream defaults

--- codium.nix
+++ codium.nix
@@ -1,11 +1,9 @@
 { config, pkgs, lib, ... }:
 {
-  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-  #   "vscode-extension-ms-toolsai-jupyter"
-  #];
-
-  home.packages = let
-    vscodeExtensions = (with pkgs.vscode-extensions; [
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = (with pkgs.vscode-extensions; [
       bbenoist.nix
       ms-python.python
       redhat.vscode-yaml
@@ -13,22 +11,14 @@
       alanz.vscode-hie-server
       justusadam.language-haskell
       vscodevim.vim
-      llvm-org.lldb-vscode
-    ])
-    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-      name = "lean";
-      vscodeExtPublisher = "jroesch";
-      publisher = "jroesch";
-      version = "0.16.22";
-      sha256 = "08fj7h6dw56s40l9fkn4xz1valxpwhc4n8vw7d44264cabcsmkrw";
-    }];
-    #vscodium-with-extensions = pkgs.vscode-with-extensions.override {
-    #  vscode = pkgs.vscodium;
-    #  inherit vscodeExtensions;
-    #};
-    #all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-  in [
-    # (all-hies.selection { selector = p: { inherit (p) ghc882 ghc865; }; })
-    # vscodium-with-extensions
-  ];
+      # llvm-org.lldb-vscode
+      alygin.vscode-tlaplus
+    ]);
+
+    userSettings = {
+      "window.zoomLevel" = 1;
+      "workbench.colorTheme" = "Default High Contrast"; 
+      "tlaplus.java.home" = "${pkgs.jdk11_headless}";
+    };
+  };
 }

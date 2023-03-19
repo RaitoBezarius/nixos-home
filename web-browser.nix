08ec323c90ff82ec78b45b2d2c349455f69c58c6
Raito Bezarius <masterancpp@gmail.com>
2023-03-19 23:32:36 +0100

    chromium: chromecast support and resolution zoom for HiDPI

--- web-browser.nix
+++ web-browser.nix
@@ -2,11 +2,13 @@
   programs.chromium = {
     enable = true;
     commandLineArgs = [
-      "--ozone-platform-hint=auto"
+      "--ozone-platform-hint=wayland"
+      "--load-media-router-component-extension=1"
     ];
     extensions = [
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
       { id = "ldlghkoiihaelfnggonhjnfiabmaficg"; } # Alt+Q switcher
+      { id = "enjjhajnmggdgofagbokhmifgnaophmh"; } # Resolution Zoom for HiDPI
       { id = "dcpihecpambacapedldabdbpakmachpb"; updateUrl = "https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/src/updates/updates.xml"; }
     ];
   };

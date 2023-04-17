827bff2686a1b3a7ce62ca9f10ab20d7d1a19a86
Raito Bezarius <masterancpp@gmail.com>
2023-04-17 10:49:52 +0200

    i3status-rs: forward symlink for config files

--- dotfiles.nix
+++ dotfiles.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
   home.file.".config/sway/config".source = ./dotfiles/sway;
   home.file.".config/waybar/config".source = ./dotfiles/waybar/config;
   home.file.".config/waybar/style.css".source = ./dotfiles/waybar/style.css;
 
   home.file.".config/i3/config".source = ./dotfiles/i3/config;
-  home.file.".config/i3status-rs/config-top.toml".source = ./dotfiles/i3status-rs/config-top.toml;
-  home.file.".config/i3status-rs/config-bottom.toml".source = ./dotfiles/i3status-rs/config-bottom.toml;
+  home.file.".config/i3status-rs/config-top.toml".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/i3status-rs/config-top.toml;
+  home.file.".config/i3status-rs/config-bottom.toml".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/i3status-rs/config-bottom.toml;
 }

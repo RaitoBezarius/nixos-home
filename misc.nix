7e80c59088e709a0cd0ba9badf0a32c0c7ac3e33
Raito Bezarius <masterancpp@gmail.com>
2023-06-02 15:17:53 +0200

    kitty: support vim as a pager for framebuffer manipulations

--- misc.nix
+++ misc.nix
@@ -1,4 +1,26 @@
 { config, pkgs, lib, ... }:
+let
+  vim-pager = pkgs.writeShellScriptBin "vim-pager" ''
+  set -eu
+
+  if [ "$#" -eq 3 ]; then
+      INPUT_LINE_NUMBER=''${1:-0}
+      CURSOR_LINE=''${2:-1}
+      CURSOR_COLUMN=''${3:-1}
+      AUTOCMD_TERMCLOSE_CMD="call cursor(max([0,''${INPUT_LINE_NUMBER}-1])+''${CURSOR_LINE}, ''${CURSOR_COLUMN})"
+  else
+      AUTOCMD_TERMCLOSE_CMD="normal G"
+  fi
+
+  exec nvim 63<&0 0</dev/null \
+      -u NONE \
+      -c "map <silent> q :qa!<CR>" \
+      -c "set shell=bash scrollback=100000 termguicolors laststatus=0 clipboard+=unnamedplus" \
+      -c "autocmd TermEnter * stopinsert" \
+      -c "autocmd TermClose * ''${AUTOCMD_TERMCLOSE_CMD}" \
+      -c 'terminal sed </dev/fd/63 -e "s/'$'\x1b''']8;;file:[^\]*[\]//g" && sleep 0.01 && printf "'$'\x1b''']2;"'
+      '';
+in
 {
   xdg.userDirs = {
     enable = true;
@@ -50,8 +72,12 @@
       package = pkgs.hack-font;
       name = "Hack";
     };
+    keybindings = {
+      "f1" = "launch --type overlay --stdin-source=@screen_scrollback ${vim-pager}/bin/vim-pager";
+    };
     settings = {
       enable_audio_bell = "no";
+      scrollback_pager = "${vim-pager}/bin/vim-pager 'INPUT_LINE_NUMBER' 'CURSOR_LINE' 'CURSOR_COLUMN'";
     };
   };
 

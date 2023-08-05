aefcbacb501767a20fb1a42110733a613debabf2
Raito Bezarius <masterancpp@gmail.com>
2023-08-06 01:12:42 +0200

    zsh: add uefi_iso_run function to debug ISO in UEFI

--- dotfiles/zsh/vm.zsh
+++ dotfiles/zsh/vm.zsh
@@ -6,6 +6,13 @@ function urun()
   nix-shell -p uefi-run -p qemu --run uefi-run --qemu-path "qemu-system-$(ARCH_STRING)" --bios-path "$OVMF/FV/OVMF.fd" "$@" --drive format=raw,file=fat:rw:${ESP_LOCATION}
 }
 
+function uefi_iso_run()
+{
+  local OVMF="$(nix-build '<nixpkgs>' -A pkgs.OVMF.fd)"
+  local ARCH_STRING="${ARCH:-x86_64}"
+  nix-shell -p qemu --run "qemu-system-$ARCH_STRING -s -global isa-debugcon.iobase=0x402 -debugcon file:uefi.log -bios \"$OVMF/FV/OVMF.fd\" -net none -nographic -boot d -cdrom $*"
+}
+
 function lurun()
 {
   local OVMF="$(nix-build -E 'let pkgs = (import <nixpkgs> {}); in (pkgs.enableDebugging (pkgs.OVMF.override { debug = true; sourceDebug = false; })).fd')"

909eb62d71936ca9077591f253c2ca6b85feee27
Raito Bezarius <masterancpp@gmail.com>
2023-11-26 18:10:57 +0100

    editor/init: stop making people angry about rust fmt

--- editor/init.lua
+++ editor/init.lua
@@ -420,3 +420,5 @@ pcall(require, 'lean')
 
 vim.g.taskwiki_taskrc_location = '/home/raito/.config/task/taskrc'
 vim.g.taskwiki_data_location = '/home/raito/.local/share/task'
+
+vim.g.rustfmt_autosave = 1

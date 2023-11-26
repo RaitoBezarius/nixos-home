4f6c48e5717aa1be0f0ec2f5bcca4f504084020e
Raito Bezarius <masterancpp@gmail.com>
2023-11-26 18:11:13 +0100

    dotfiles: put i3status-rs links in the store again

--- dotfiles.nix
+++ dotfiles.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
   home.file.".config/i3/config".source = ./dotfiles/i3/config;
-  home.file.".config/i3status-rs/config-top.toml".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/i3status-rs/config-top.toml;
-  home.file.".config/i3status-rs/config-bottom.toml".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/i3status-rs/config-bottom.toml;
+  home.file.".config/i3status-rs/config-top.toml".source = ./dotfiles/i3status-rs/config-top.toml;
+  home.file.".config/i3status-rs/config-bottom.toml".source = ./dotfiles/i3status-rs/config-bottom.toml;
 }
